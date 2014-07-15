0977326bdc3b6ce3ad38ad1c13f671ba96dba972
Roland Batovski <roland.batovski@gmail.com>
2014-07-15 09:57:19 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BookLendingApp::Application.config.secret_key_base = '1e05ac261b860f0a3f4c87ff441fee25ed99b628c4f47c164dd337257f72e4f82870664b4e7c22413609e51278f5e2438d97b1cf5180c05a3af0f4086f25093a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BookLendingApp::Application.config.session_store :cookie_store, key: '_book_lending_app_session'
